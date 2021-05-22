108d868d485ca4d41f7e282bf8a1468599d02d9e
Mustafa Alkamali <you@example.com>
2021-05-22 23:36:49 +0300

    Object Literal with 'New' keyword and Object.create()

--- script.js
+++ script.js
@@ -2,20 +2,30 @@
   Defining Object
   [1] Object Literal
   [2] With New Keyword
+  [3] With Object.create()
 */
 
-let user = new Object();
-
-user.firstName = "Mustafa";
-user.lastName = "Alkamali";
-user['age'] = 20;
-
-user.getFullName = function() {
-    return `Full Name is: ${user.firstName} ${user.lastName}`;
+let mainObj = {
+    hasDiscount: true,
+    showMsg: function() {
+        return `You${this.hasDiscount ? "" : " Don't"} Have a Discount`;
+    }
 }
 
-console.log(user);
-console.log(user.firstName);
-console.log(user.lastName);
-console.log(user['age']);
-console.log(user.getFullName());
+console.log(mainObj.hasDiscount);
+console.log(mainObj.showMsg());
+console.log('---------------');
+
+let otherObj = Object.create(mainObj);
+
+otherObj.hasDiscount = false;
+console.log(otherObj.hasDiscount);
+console.log(otherObj.showMsg());
+console.log('---------------');
+
+let lastObj = Object.create(mainObj);
+
+// lastObj.hasDiscount = true;
+console.log(lastObj.hasDiscount);
+console.log(lastObj.showMsg());
+console.log('---------------');

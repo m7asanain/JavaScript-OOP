19eca2969ee0452802c9a3906c2c7f25d3682ba6
Mustafa Alkamali <m7aanain@gmail.com>
2021-06-01 23:06:59 +0300

    Object.defineProperties & Trainings

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 /*
-  Object Metadata
-  writable
-  enumerable
-  configurable
-  ============
-  Object.defineProperty(obj, prop, descriptor)
+  Object.defineProperties & Trainings
 */
 
 const myObject = {
@@ -12,28 +7,44 @@ const myObject = {
   b: 2,
 }
 
-Object.defineProperty(myObject, "c", {
+Object.defineProperty(myObject, "a", {
   writable: true,       // false : will not change the value
-  enumerable: true,     // false : will not include added value 
+  enumerable: false,     // false : will not include added value 
   configurable: false,  // false : will not delete a value
+  value: 500,
+});
+
+Object.defineProperty(myObject, "c", {
   value: 3,
 });
 
-// Object.defineProperty(myObject, "c", {
-//   writable: true,
-// });
+myObject.d = 4;
 
-console.log(delete myObject.c);
-myObject.c = 500;
+Object.defineProperties(myObject, {
+  e: {
+    writable: false,
+    enumerable: false,
+    configurable: false,
+    value: 5,
+  },
+  f: {
+    writable: false,
+    enumerable: false,
+    configurable: false,
+    value: 6,
+  },
+  g: {
+    writable: true,
+    enumerable: true,
+    configurable: true,
+    value: 7,
+  }
+});
 
 console.log(myObject);
-
-console.log('#'.repeat(10));
-
-for (let prop in myObject) {
-  console.log(prop, myObject[prop]);
-}
-
-console.log('#'.repeat(10));
+console.log(Object.getOwnPropertyDescriptor(myObject, "a"));
+console.log(Object.getOwnPropertyDescriptor(myObject, "c"));
+console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
 
 console.log(Object.getOwnPropertyNames(myObject));
+console.log(Object.keys(myObject));   // return vlaues that only can loops / enumerable: false
